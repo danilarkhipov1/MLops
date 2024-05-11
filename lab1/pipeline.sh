561004b935cdf54df9ac6da8de09d01d3b861768
danilarkhipov1 <you@example.com>
2024-05-11 16:57:16 +0300

    add hometask

---
+++ lab1/pipeline.sh
@@ -0,0 +1,5 @@
+python3 ./MLPops urfu/lab1/data_creation.py
+python3 ./MLPops urfu/lab1/model_preprocessing.py
+python3 ./MLPops urfu/lab1/model_preparation.py
+python3 ./MLPops urfu/lab1/model_testing.py
+python3 ./MLPops urfu/lab1/cleaner.py
